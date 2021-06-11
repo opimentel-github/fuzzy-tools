498ad62555c5badbd8d766a4c540a99d59401dae
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-06-11 11:21:44 -0400

    32 copies

--- fuzzytools/datascience/metrics.py
+++ fuzzytools/datascience/metrics.py
@@ -6,15 +6,20 @@ from sklearn import metrics as skmetrics
 import numpy as np
 import math
 from nested_dict import nested_dict
+from copy import copy, deepcopy
+
+EPS = C_.EPS
+
+# https://stackoverflow.com/questions/53977031/precision-score-does-not-match-with-metrics-formula
+# https://machinelearningmastery.com/roc-curves-and-precision-recall-curves-for-classification-in-python/#:~:text=ROC%20Curves%20and%20AUC%20in%20Python,-We%20can%20plot&text=The%20function%20takes%20both%20the,for%20each%20threshold%20and%20thresholds.
 
 ###################################################################################################################################################
 
 def _check_prob(y_pred_p, class_names):
 	assert len(y_pred_p.shape)==2
 	assert y_pred_p.shape[-1]==len(class_names)
-	assert np.all(y_pred_p>=0)
-	assert np.all(y_pred_p<=1)
-	#assert np.all((1-np.sum(y_pred_p, axis=-1))**2<=C_.EPS)
+	assert np.all(y_pred_p>=0) and np.all(y_pred_p<=1)
+	#assert np.all((1-np.sum(y_pred_p, axis=-1))**2<=EPS)
 
 def get_cm(_y_pred, _y_target, class_names,
 	pred_is_onehot:bool=False,
@@ -49,10 +54,10 @@ class BinBatchCM():
 		assert len(class_names)==2
 		assert pos_label in [0, 1]
 
-		self.y_pred = y_pred.copy()
-		self.y_target = y_target.copy()
+		self.y_pred = copy(y_pred)
+		self.y_target = copy(y_target)
 		self.class_names = class_names
-		self.pos_probability = pos_probability.copy()
+		self.pos_probability = copy(pos_probability)
 		self.pos_label = pos_label
 		self.reset()
 
@@ -79,19 +84,19 @@ class BinBatchCM():
 
 	def get_precision(self):
 		tp, fn, fp, tn = self.get_cm_elements()
-		return tp/(tp+fp+C_.EPS)
+		return tp/(tp+fp+EPS)
 
 	def get_recall(self):
 		tp, fn, fp, tn = self.get_cm_elements()
-		return tp/(tp+fn+C_.EPS)
+		return tp/(tp+fn+EPS)
 
 	def get_specifity(self):
 		tp, fn, fp, tn = self.get_cm_elements()
-		return tn/(tn+fp+C_.EPS)
+		return tn/(tn+fp+EPS)
 
 	def get_accuracy(self):
 		tp, fn, fp, tn = self.get_cm_elements()
-		return (tp+tn)/(tp+fn+fp+tn+C_.EPS)
+		return (tp+tn)/(tp+fn+fp+tn+EPS)
 
 	def get_gmean(self):
 		recall = self.get_recall()
@@ -101,48 +106,85 @@ class BinBatchCM():
 	def get_dpower(self):
 		recall = self.get_recall()
 		specifity = self.get_specifity()
-		x = recall/(1-recall+C_.EPS)
-		y = specifity/(1-specifity+C_.EPS)
-		return math.sqrt(3.)/math.pi*(math.log(x+C_.EPS)+math.log(y+C_.EPS))
+		x = recall/(1-recall+EPS)
+		y = specifity/(1-specifity+EPS)
+		return math.sqrt(3.)/math.pi*(math.log(x+EPS)+math.log(y+EPS))
 
 	def get_fscore(self, beta):
 		precision = self.get_precision()
 		recall = self.get_recall()
-		return (1+beta**2)*(precision*recall)/((beta**2*precision)+recall+C_.EPS)
+		return (1+beta**2)*(precision*recall)/((beta**2*precision)+recall+EPS)
 
 	def get_f1score(self):
 		return self.get_fscore(1)
 
-	def get_xentropy(self):
+	def _get_bin_p(self):
 		p = self.pos_probability
-		return np.mean(-1*np.log(p+C_.EPS))
+		y_target = self.y_target==self.pos_label
+		assert np.sum(y_target)<len(y_target), 'needs samples from both classes'
+		# print(p, y_target)
+		return p, y_target
+
+	def get_xentropy(self):
+		p, y_target = self._get_bin_p()
+		return np.mean(-1*np.log(p+EPS))
+
+	def get_prc(self):
+		p, y_target = self._get_bin_p()
+		precision, recall, thresholds = skmetrics.precision_recall_curve(y_target, p)
+		d = {
+			'recall':recall,
+			'precision':precision,
+			'thresholds':thresholds,
+			'_len':len(recall),
+			}
+		return d
 
 	def get_prauc(self):
-		p = self.pos_probability
-		y_target = self.y_target==self.pos_label
+		p, y_target = self._get_bin_p()
 		return skmetrics.average_precision_score(y_target, p, pos_label=1)
 
+	def get_rocc(self):
+		p, y_target = self._get_bin_p()
+		fpr, tpr, thresholds = skmetrics.roc_curve(y_target, p)
+		d = {
+			'fpr':fpr,
+			'tpr':tpr,
+			'thresholds':thresholds,
+			'_len':len(fpr),
+			}
+		return d
+
 	def get_rocauc(self):
-		p = self.pos_probability
-		y_target = self.y_target==self.pos_label
+		p, y_target = self._get_bin_p()
 		return skmetrics.roc_auc_score(self.y_target, p)
 
 ###################################################################################################################################################
-#https://stackoverflow.com/questions/53977031/precision-score-does-not-match-with-metrics-formula
+
 def get_multiclass_metrics(_y_pred_p, _y_target, class_names,
-	metrics=['accuracy', 'precision', 'recall', 'f1score', 'prauc', 'rocauc'],#, 'xentropy'],
+	metrics=[
+		'precision',
+		'recall',
+		'f1score',
+		'accuracy',
+		'prc',
+		'prauc',
+		'rocc',
+		'rocauc',
+		'xentropy',
+		],
 	target_is_onehot:bool=False,
 	):
 	### checks
-	y_target = _y_target.copy()
-	y_pred_p = _y_pred_p.copy()
+	y_pred_p = copy(_y_pred_p)
+	y_target = copy(_y_target)
 	_check_prob(y_pred_p, class_names)
 	assert len(class_names)>2
 	assert len(y_pred_p)==len(y_target)
 
 	### compute for each binary cm case
-	y_target = y_target.argmax(axis=-1) if target_is_onehot else np.copy(y_target)
-	y_pred = y_pred_p.argmax(axis=-1)
+	y_target = y_target.argmax(axis=-1) if target_is_onehot else y_target
+	y_pred = y_pred_p.argmax(axis=-1) # predicted is the max prob by default!
 	metrics_cdict = nested_dict()
 	for kc,c in enumerate(class_names):
 		y_target_c = ((y_target==kc)).astype(int)
@@ -151,9 +193,6 @@ def get_multiclass_metrics(_y_pred_p, _y_target, class_names,
 		bin_bach_cm = BinBatchCM(y_pred_c, y_target_c, [f'non-{c}', f'c'], pos_probability=pos_probability_c, pos_label=1)
 		for m in metrics:
 			metrics_cdict[c][m] = getattr(bin_bach_cm, f'get_{m}')()
-
-		#print(metrics_cdict)
-		#assert 0
 	metrics_cdict = metrics_cdict.to_dict()
 
 	### get cm
@@ -165,6 +204,8 @@ def get_multiclass_metrics(_y_pred_p, _y_target, class_names,
 	total_samples = sum(support[c] for c in class_names)
 	metrics_dict = {}
 	for m in metrics:
+		if isinstance(metrics_cdict[class_names[0]][m], dict):
+			continue
 		metrics_dict[f'b-{m}'] = sum([1*metrics_cdict[c][m] for c in class_names])/len(class_names)
 		metrics_dict[f'w-{m}'] = sum([support[c]*metrics_cdict[c][m] for c in class_names])/total_samples
 		
