6c871d90400115d7d61b17399fd3d8e8675398d8
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-06-11 16:50:45 -0400

    32 copies

--- fuzzytools/cuteplots/lines.py
+++ fuzzytools/cuteplots/lines.py
@@ -7,10 +7,11 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 INTERP_N = 1000
+PERCENTILE_PLOT = 95
 
 ###################################################################################################################################################
 
-def plot_multiple_lines(ax, xs, ys,
+def multiple_lines(ax, xs, ys,
 	*args,
 	**kwargs,
 	):
@@ -20,12 +21,18 @@ def plot_multiple_lines(ax, xs, ys,
 		ax.plot(x, y, *args, **kwargs)
 	return ax
 
-def plot_fill_beetween(ax, xs, ys,
+def fill_beetween(ax, xs, ys,
 	interp_n=INTERP_N,
 	interp_kind='linear',
-	*args,
-	**kwargs,
+	percentile=PERCENTILE_PLOT,
+	plot_median=True,
+	fill_args=[],
+	fill_kwargs={},
+	median_args=[],
+	median_kwargs={},
 	):
+	assert percentile>=0 and percentile<=100
+
 	x_min = min([x.min() for x in xs])
 	x_max = max([x.max() for x in xs])
 	new_x = np.linspace(x_min, x_max, interp_n)
@@ -37,5 +44,10 @@ def plot_fill_beetween(ax, xs, ys,
 		new_ys += [new_y[None]]
 	
 	new_ys = np.concatenate(new_ys, axis=0)
-	ax.fill_between(new_x, np.nanmin(new_ys, axis=0), np.nanmax(new_ys, axis=0), *args, **kwargs)
+	lower_y = np.nanpercentile(new_ys, 100-percentile, axis=0)
+	median_y = np.nanpercentile(new_ys, 50, axis=0)
+	upper_y = np.nanpercentile(new_ys, percentile, axis=0)
+	ax.fill_between(new_x, lower_y, upper_y, *fill_args, **fill_kwargs)
+	if plot_median:
+		ax.plot(new_x, median_y, *median_args, **median_kwargs)
 	return ax
