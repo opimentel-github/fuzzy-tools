8389466c243d21cb6e6d31d48837140707d2b83f
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-06-28 09:57:38 -0400

    scatter

--- fuzzytools/matplotlib/scatters.py
+++ fuzzytools/matplotlib/scatters.py
@@ -7,13 +7,13 @@ from copy import copy, deepcopy
 
 ###################################################################################################################################################
 
-def scatter_plot(ax, x, y_true, class_names, scatter_kwargs,
+def scatter(ax, x, y_true, class_names, scatter_kwargs,
 	sort_by_count=True,
 	add_class_label=True,
 	):
 	assert len(x.shape)==2
 	assert x.shape[-1]==2
-	assert len(x)==len(y)
+	assert len(x)==len(y_true)
 	
 	y_uniques, counts = np.unique(y_true, return_counts=True)
 	plot_order = np.argsort(counts)[::-1] if sort_by_count else list(range(0, len(y_uniques)))
@@ -21,14 +21,17 @@ def scatter_plot(ax, x, y_true, class_names, scatter_kwargs,
 	
 	for k in plot_order:
 		y_unique = y_uniques[k]
+		count = counts[k]
 		class_name = class_names[k]
+		if count==0:
+			continue
 		valid_idxs = np.where(y_true==y_unique)[0]
 		_x = x[valid_idxs]
 		_y_true = [valid_idxs]
 		_scatter_kwargs = deepcopy(scatter_kwargs[class_name])
 		if add_class_label:
 			_scatter_kwargs.update({
-				'label':_scatter_kwargs.get('label', '')+f'[{class_name}]',
+				'label':_scatter_kwargs.get('label', '')+f' [{class_name}]',
 				})
 		ax.scatter(_x[:,0], _x[:,1], **_scatter_kwargs)
 	return ax
