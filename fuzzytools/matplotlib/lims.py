5f7433622f96e1cbf04c691ca508278788d2f79d
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-07-03 12:45:30 -0400

    jeje

--- fuzzytools/matplotlib/lims.py
+++ fuzzytools/matplotlib/lims.py
@@ -10,39 +10,42 @@ EXTENDED_PERCENT = 0.1
 
 ###################################################################################################################################################
 
-def get_xlim(_x_values,
-	x_extended_percent=EXTENDED_PERCENT,
-	):
+def get_xlim(_x_values, axis_extended_percent):
 	x_values = np.concatenate(_x_values, axis=0) if isinstance(_x_values, list) else copy(_x_values)
 	assert len(x_values.shape)==1
 
 	x_min, x_max = x_values.min(), x_values.max()
 	dx = x_max-x_min
-	x_margin = x_extended_percent*dx
+	x_margin = axis_extended_percent*dx
 	xlim = (x_min-x_margin, x_max+x_margin)
 	return xlim
 
-def get_lims(_xy_values,
-	x_extended_percent=EXTENDED_PERCENT,
-	y_extended_percent=EXTENDED_PERCENT,
-	):
-	xy_values = np.concatenate(_xy_values, axis=0) if isinstance(_xy_values, list) else copy(_xy_values)
-	assert len(xy_values.shape)==2
-	assert xy_values.shape[-1]==2
+###################################################################################################################################################
 
-	xlim = get_xlim(xy_values[:,0],
-		x_extended_percent,
-		)
-	ylim = get_xlim(xy_values[:,1],
-		y_extended_percent,
-		)
-	return xlim, ylim
+class AxisLims(object):
+	def __init__(self, axis_clip_values,
+		axis_extended_percent=EXTENDED_PERCENT,
+		):
+		self.axis_clip_values = axis_clip_values
+		self.axis_extended_percent = {k:axis_extended_percent for k in axis_clip_values.keys()} if not isinstance(axis_extended_percent, dict) else axis_extended_percent
+		self.reset()
 
-def set_lims(ax, x_values, y_values,
-	x_extended_percent=EXTENDED_PERCENT,
-	y_extended_percent=EXTENDED_PERCENT,
-	):
-	xlim, ylim = get_lims(x_values, y_values,)
-	ax.set_xlim(xlim)
-	ax.set_ylim(ylim)
-	return ax
+	def reset(self):
+		self.axis_d = {k:[] for k in self.axis_clip_values.keys()}
+		pass
+
+	def append(self, axis_name, axis_value:tuple):
+		self.axis_d[axis_name] += [axis_value]
+
+	def get_axis_lim(self, axis_name):
+		axis_extended_percent = self.axis_extended_percent[axis_name]
+		axis_clip_values = self.axis_clip_values[axis_name]
+
+		axis_lim = get_xlim(self.axis_d[axis_name], axis_extended_percent)
+		axis_lim = np.clip(axis_lim, axis_clip_values[0], axis_clip_values[1])
+		return axis_lim
+
+	def set_axis_lims(self, ax):
+		for k in self.axis_d.keys():
+			getattr(ax, f'set_{k}lim')(self.get_axis_lim(k))
+		return ax
