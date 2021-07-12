e59843e8c792351db0aef0abbde68f750f9a9dce
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-07-11 22:40:47 -0400

    jeje

--- fuzzytools/datascience/missclassifications.py
+++ fuzzytools/datascience/missclassifications.py
@@ -19,6 +19,7 @@ def plot_missclassifications(_y_pred_p, _y_true, class_names,
 	title=None,
 	legend_loc='upper right',
 	fontsize=10,
+	pred_prob_th=None,
 	):
 	### checks
 	assert len(class_names)>2
@@ -35,18 +36,21 @@ def plot_missclassifications(_y_pred_p, _y_true, class_names,
 		for k,idx in enumerate(valid_idxs):
 			obj_y_pred = y_pred[idx]
 			obj_y_pred_c = class_names[obj_y_pred]
-			p = y_pred_p[idx][obj_y_pred]
+			obj_y_pred_p = y_pred_p[idx][obj_y_pred]
 			correct_classification = obj_y_pred==kc
 			obj_id = None if obj_ids is None else obj_ids[idx]
 			if correct_classification:
-				ax.plot(k, p, 'o', c='k')
+				ax.plot(k, obj_y_pred_p, 'o', c='k')
 			else:
-				ax.plot(k, p, 'D', c='r')
-				txt = f'{obj_y_pred_c}' if obj_id is None else f'{obj_id} [{obj_y_pred_c}]'
-				ax.text(k, p, txt, rotation=90, ha='center', va='top', fontsize=fontsize)
+				ax.plot(k, obj_y_pred_p, 'D', c='r')
+				if pred_prob_th is None or obj_y_pred_p>=pred_prob_th:
+					txt = f'{obj_y_pred_c}' if obj_id is None else f'{obj_id} [{obj_y_pred_c}]'
+					ax.text(k, obj_y_pred_p, txt, rotation=90, ha='center', va='top', fontsize=fontsize)
 
 		ax.plot([None], [None], 'o', c='k', label=f'correct-classification for y_true={c}')
 		ax.plot([None], [None], 'D', c='r', label=f'miss-classification for y_true={c}')
+		if not pred_prob_th is None:
+			ax.axhline(pred_prob_th, linestyle='--', c='r', label=f'y_pred_threshold={pred_prob_th}')
 
 		ax.set_ylabel('y_pred prob')
 		ax.set_ylim([0, 1])
