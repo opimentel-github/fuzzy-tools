5631cc0c352a9954e26b794d4eddb7544d5412f6
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-08-15 15:39:15 -0400

    tests

--- fuzzytools/datascience/statistical_tests.py
+++ fuzzytools/datascience/statistical_tests.py
@@ -6,10 +6,31 @@ import numpy as np
 from scipy import stats
 from ..strings import xstr
 from .xerror import XError
+from ..dataframes import DFBuilder
 
 TH_PVALUE = 0.05
 LOWER_BOUND = 0.001
 UPPER_BOUND = 0.99
+
+###################################################################################################################################################
+
+def _check_values(values):
+	assert isinstance(values, list) or isinstance(values, np.ndarray)
+	return values
+
+def grid_is_greater_test(values_dict, test):
+	df_builder = DFBuilder()
+	for key1 in values_dict.keys():
+		d = {}
+		values1 = _check_values(values_dict[key1])
+		for key2 in values_dict.keys():
+			values2 = _check_values(values_dict[key2])
+			is_significant_greater, pvalue = test(values1, values2)
+			d[key2] = pvalue
+
+		df_builder.append(key1, d)
+
+	return df_builder.get_df()
 
 ###################################################################################################################################################
 
@@ -24,11 +45,11 @@ def format_pvalue(pvalue,
 		txt = f'p>{xstr(upper_bound)}'
 	return txt
 
-def welch_test_is_greater(_x1, _x2,
+def welch_is_greater_test(_x1, _x2,
 	th_pvalue=TH_PVALUE,
 	lower_bound=LOWER_BOUND,
 	upper_bound=UPPER_BOUND,
-	verbose=1,
+	verbose=0,
 	):
 	if np.mean(_x1)>np.mean(_x2):
 		x1 = _x1
@@ -36,18 +57,18 @@ def welch_test_is_greater(_x1, _x2,
 	else:
 		x1 = _x2
 		x2 = _x1
-	return _welch_test_is_greater(x1, x2,
+	return _welch_is_greater_test(x1, x2,
 		th_pvalue=th_pvalue,
 		lower_bound=lower_bound,
 		upper_bound=upper_bound,
 		verbose=verbose,
 		)
 
-def _welch_test_is_greater(_x1, _x2,
+def _welch_is_greater_test(_x1, _x2,
 	th_pvalue=TH_PVALUE,
 	lower_bound=LOWER_BOUND,
 	upper_bound=UPPER_BOUND,
-	verbose=1,
+	verbose=0,
 	):
 	'''
 	check x1>x2 with statistical significance
