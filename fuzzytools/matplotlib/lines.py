6f3d091c59d7000ede094d06e25d1488c54beb2a
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-10-12 07:36:10 -0300

    ee

--- fuzzytools/matplotlib/lines.py
+++ fuzzytools/matplotlib/lines.py
@@ -15,11 +15,18 @@ def multiple_lines(ax, xs, ys,
 	*args,
 	**kwargs,
 	):
+	new_ys = []
 	for x,y in zip(xs, ys):
 		assert x.shape==y.shape
 		assert len(x.shape)==1
 		ax.plot(x, y, *args, **kwargs)
-	return ax
+		new_ys += [new_y[None]]
+	new_ys = np.concatenate(new_ys, axis=0)
+	lower_y = np.min(new_ys, axis=0)
+	median_y = np.nanpercentile(new_ys, 50, axis=0)
+	upper_y = np.max(new_ys, axis=0)
+	yrange = [np.max(upper_y), np.min(lower_y)]
+	return ax, median_y, yrange
 
 def fill_beetween(ax, xs, ys,
 	interp_n=INTERP_N,
@@ -33,29 +40,30 @@ def fill_beetween(ax, xs, ys,
 	return_extras=False,
 	):
 	new_x = np.concatenate([x for x in xs], axis=0)
+	new_x = np.unique(new_x, axis=0)
 	new_x = np.sort(new_x)
-	new_ys = []
-	for x,y in zip(xs, ys):
-		assert x.shape==y.shape
-		assert len(x.shape)==1
-		new_y = interp1d(x, y, kind=interp_kind, bounds_error=False)(new_x)
-		new_ys += [new_y[None]]
-	
-	new_ys = np.concatenate(new_ys, axis=0)
 	if percentile is None:
-		ax = multiple_lines(ax, xs, ys, *median_args, **median_kwargs)
+		ax, median_y, yrange = multiple_lines(ax, xs, ys, *median_args, **median_kwargs)
 	else:
+	new_ys = []
+		for x,y in zip(xs, ys):
+			assert x.shape==y.shape
+			assert len(x.shape)==1
+			new_y = interp1d(x, y, kind=interp_kind, bounds_error=False)(new_x)
+			new_ys += [new_y[None]]
+		
+		new_ys = np.concatenate(new_ys, axis=0)
 		assert percentile>=0 and percentile<=100
 		lower_y = np.nanpercentile(new_ys, 100-percentile, axis=0)
 		median_y = np.nanpercentile(new_ys, 50, axis=0)
 		upper_y = np.nanpercentile(new_ys, percentile, axis=0)
+		yrange = [np.max(upper_y), np.min(lower_y)]
 		if not percentile==50:
 			ax.fill_between(new_x, lower_y, upper_y, *fill_args, **fill_kwargs)
 		if plot_median:
 			ax.plot(new_x, median_y, *median_args, **median_kwargs)
 	
 	if return_extras:
-		yrange = [np.max(upper_y), np.min(lower_y)]
 		return ax, new_x, median_y, yrange
 	else:
 		return ax
