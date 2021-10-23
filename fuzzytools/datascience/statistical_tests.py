6d9c83d2c508cb6ed19875b93b2b45ec8f1a770a
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-10-23 17:48:42 -0300

    lc

--- fuzzytools/datascience/statistical_tests.py
+++ fuzzytools/datascience/statistical_tests.py
@@ -8,11 +8,17 @@ from ..strings import xstr
 from .xerror import XError
 from ..dataframes import DFBuilder
 
-TH_PVALUE = 0.05
-LOWER_BOUND = 0.001
-UPPER_BOUND = 0.99
-PVALUE_CHAR = '$p^*$'
+NORMAL_TH_PVALUE = .05
 N_DECIMALS = _C.N_DECIMALS
+PVALUE_CHAR = '$p_v$'
+NULL_CHAR = ''
+PVALUE_SYMBOLS = {
+	'***':[-np.inf, .001],
+	'**':[.001, .01],
+	'*':[.01, .05],
+	'+':[.05, .01],
+	'':[.01, np.inf],
+	}
 
 ###################################################################################################################################################
 
@@ -20,9 +26,33 @@ def _check_values(values):
 	assert isinstance(values, list) or isinstance(values, np.ndarray)
 	return values
 
+def get_pvalue_symbol(pvalue,
+	get_upper_bound=False,
+	):
+	symbols = list(PVALUE_SYMBOLS.keys())
+	for s in symbols:
+		lower_bound = PVALUE_SYMBOLS[s][0]
+		upper_bound = PVALUE_SYMBOLS[s][-1]
+		if pvalue>lower_bound and pvalue<=upper_bound:
+			if get_upper_bound:
+				return upper_bound, s
+			else:
+				return s
+
+def get_pvalue_symbols():
+	symbols = list(PVALUE_SYMBOLS.keys())
+	return {f'<={PVALUE_SYMBOLS[s][-1]}':s for s in symbols}
+
+def is_normal(x,
+	th_pvalue=NORMAL_TH_PVALUE,
+	):
+	pvalue = stats.shapiro(x).pvalue
+	return not pvalue<th_pvalue
+
 def grid_is_greater_test(values_dict, test,
 	n_decimals=N_DECIMALS,
 	):
+	print(get_pvalue_symbols())
 	df_builder = DFBuilder()
 	for key1 in values_dict.keys():
 		d = {}
@@ -30,12 +60,12 @@ def grid_is_greater_test(values_dict, test,
 		for key2 in values_dict.keys():
 			values2 = _check_values(values_dict[key2])
 			if np.mean(values1)<np.mean(values2):
-				d[key2] = '-'
+				d[key2] = NULL_CHAR
 			else:
-				is_significant_greater, pvalue, pvalue_txt, diff = test(values1, values2,
+				diff, pvalue, pvalue_txt = test(values1, values2,
 					n_decimals=n_decimals,
 					)
-				d[key2] = f'{pvalue_txt} ($\\Delta$={xstr(diff)})'
+				d[key2] = f'{pvalue_txt}'
 
 		df_builder.append(key1, d)
 
@@ -43,32 +73,22 @@ def grid_is_greater_test(values_dict, test,
 
 ###################################################################################################################################################
 
-def format_pvalue(pvalue,
-	th_pvalue=TH_PVALUE,
-	lower_bound=LOWER_BOUND,
-	upper_bound=UPPER_BOUND,
-	):
-	if pvalue<lower_bound:
-		txt = f'{PVALUE_CHAR}<{xstr(lower_bound)}'
-	elif pvalue>upper_bound:
-		txt = f'{PVALUE_CHAR}>{xstr(upper_bound)}'
-	else:
-		txt = f'{PVALUE_CHAR}={xstr(pvalue)}'
-	if pvalue<th_pvalue:
-		txt = f'{txt}<{xstr(th_pvalue)}'
+def format_pvalue(pvalue, diff):
+	if pvalue is None:
+		return NULL_CHAR
+	txt = f'$\Delta$={diff}{get_pvalue_symbol(pvalue)}; {PVALUE_CHAR}={xstr(pvalue)}'
 	return txt
 
 def welch_is_greater_test(_x1, _x2,
-	th_pvalue=TH_PVALUE,
-	lower_bound=LOWER_BOUND,
-	upper_bound=UPPER_BOUND,
 	verbose=0,
 	sort=False,
 	n_decimals=N_DECIMALS,
 	):
 	'''
-	check x1>x2 with statistical significance
+	check if mean(x1)>mean(x2) with statistical significance
 	'''
+	assert len(_x1)>=1 and len(_x1.shape)==1
+	assert len(_x2)>=1 and len(_x2.shape)==1
 	if sort:
 		if np.mean(_x1)>np.mean(_x2):
 			x1 = np.array(_x1)
@@ -79,15 +99,7 @@ def welch_is_greater_test(_x1, _x2,
 	else:
 		x1 = np.array(_x1)
 		x2 = np.array(_x2)
-	assert len(x1)>=1 and len(x1.shape)==1
-	assert len(x2)>=1 and len(x2.shape)==1
-	statistic, pvalue = stats.ttest_ind(x1, x2,
-		alternative='greater', # two-sided less greater
-		equal_var=False, # welch
-		)
-	
-	is_significant_greater = pvalue<th_pvalue
-	pvalue_txt = format_pvalue(pvalue, th_pvalue, lower_bound, upper_bound)
+
 	mean_x1 = xstr(np.mean(x1),
 		n_decimals=n_decimals,
 		remove_zero=False,
@@ -98,12 +110,15 @@ def welch_is_greater_test(_x1, _x2,
 		)
 	diff = float(mean_x1)-float(mean_x2)
 	assert diff>=0
+	pvalue = None
+	are_normal = is_normal(x1) and is_normal(x2)
 	if verbose:
-		print(f'th-p-value={th_pvalue}')
-		print(f'x1={XError(x1)}')
-		print(f'x2={XError(x2)}')
-		if is_significant_greater:
-			print(f'x1>x2 with {pvalue_txt}')
-		else:
-			print(f'!x1>x2 with {pvalue_txt}')
-	return is_significant_greater, pvalue, pvalue_txt, diff
+		print(f'are_normal={are_normal}')
+	if are_normal:
+		statistic, pvalue = stats.ttest_ind(x1, x2,
+			alternative='greater', # two-sided less greater
+			equal_var=False, # welch
+			)
+
+	pvalue_txt = format_pvalue(pvalue, diff)
+	return diff, pvalue, pvalue_txt
