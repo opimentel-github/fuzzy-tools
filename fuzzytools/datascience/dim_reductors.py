678e35d0d206aefe87be1ae1f183e1683a994239
opimentel-tesla <oscarlo.pimentel@gmail.com>
2022-02-08 11:17:55 -0300

    latex

--- fuzzytools/datascience/dim_reductors.py
+++ fuzzytools/datascience/dim_reductors.py
@@ -34,13 +34,14 @@ class DimReductor():
 	def fit(self, x,
 		drop_duplicates=False,
 		normal_std=0,
+		reduction_map_kwargs={},
 		):
 		new_x = np.concatenate(x, axis=0) if type(x)==list else copy(x)
 		_check(new_x)
 			
 		if drop_duplicates:
 			print('deleting duplicates', new_x.shape)
-			new_x = np.unique(new_x, axis=0)
+			new_x = np.unique(new_x, axis=0) # drop duplicated
 			print(new_x.shape)
 
 		new_x = new_x+np.random.normal(0, normal_std, size=new_x.shape) if normal_std>0 else new_x
@@ -49,10 +50,12 @@ class DimReductor():
 			if not hasattr(self, 'pca'):
 				self.pca = PCA(n_components=self.inter_pca_dims )
 			new_x = self.pca.fit_transform(new_x)
-		_ = self.reduction_map.fit(new_x)
+		_ = self.reduction_map.fit(new_x, **reduction_map_kwargs)
 		self.fitted = True
 
-	def transform(self, x):
+	def transform(self, x,
+		reduction_map_kwargs={},
+		):
 		assert self.fitted
 		new_x = copy(x)
 		_check(new_x)
@@ -60,10 +63,16 @@ class DimReductor():
 		new_x = self.scaler.transform(new_x)
 		if not self.inter_pca_dims is None:
 			new_x = self.pca.transform(new_x)
-		new_x = self.reduction_map.transform(new_x)
+		new_x = self.reduction_map.transform(new_x, **reduction_map_kwargs)
 		return new_x
 
-	def fit_transform(self, x):
-		self.fit(x)
-		new_x = self.transform(x)
+	def fit_transform(self, x,
+		reduction_map_kwargs={},
+		):
+		self.fit(x,
+			reduction_map_kwargs=reduction_map_kwargs,
+			)
+		new_x = self.transform(x,
+			reduction_map_kwargs=reduction_map_kwargs,
+			)
 		return new_x
